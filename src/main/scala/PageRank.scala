c03dafe739720e84f3d54d5202967d5b7d6624e9
Quinn Cypher <TMQCypher.git@gmail.com>
2021-04-06 12:22:49 -0400

    Implemented equal(pages: Map[String, WebPage]): Map[String, Double]

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -2,9 +2,9 @@ import scala.util.Random
 import scala.collection.parallel.CollectionConverters._
 
 object PageRank {
-    def equal(pages: Map[String, WebPage]): Map[String, Double] = {
-        // TODO: complete implementation
-    }
+
+    def equal(pages: Map[String, WebPage]): Map[String, Double] =
+      (for(s <- pages.keys) yield (s -> 1.0)).toMap
 
     def indegree(pages: Map[String, WebPage]): Map[String, Double] = {
         // TODO: complete implementation
