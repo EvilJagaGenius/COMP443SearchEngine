0aff1b835f77a756eac50bd7cb3878c1b5aee182
PurplWarrior22 <44842522+PurplWarrior22@users.noreply.github.com>
2021-04-14 18:08:51 -0400

    Added query detecting and WebPages are now reconized for me

--- src/main/scala/AskWillie.scala
+++ src/main/scala/AskWillie.scala
@@ -16,40 +16,59 @@ object AskWillie {
         val pages: Map[String, WebPage] = mapWebPages(loadWebPages)
 
         //Ranking Pages with All the Ranking Algorithms
-        val pagesEqualRank = PageRank.equal(pages)
-        val pagesIndegreeRank = PageRank.indegree(pages)
-        val pagesPageRank = PageRank.pagerank(pages);
+        val pagesEqualRank: Map[String, Double] = PageRank.equal(pages)
+        val pagesIndegreeRank: Map[String, Double] = PageRank.indegree(pages)
+        val pagesPageRank: Map[String, Double] = PageRank.pagerank(pages);
 
         //Min-Max Normalization Of Ranking
-        val rankIndegreeList = for(page <- pagesIndegreeRank.values) yield page
-        val minIndegreeRank = rankIndegreeList.foldLeft(Double.MaxValue)(Math.min(_, _))
-        val maxIndegreeRank = rankIndegreeList.foldLeft(Double.MinValue)(Math.max(_, _))
-        val rankPagerankList = for(page <- pagesPageRank.values) yield page
-        val minPagerankRank = rankPagerankList.foldLeft(Double.MaxValue)(Math.min(_, _))
-        val maxPagerankRank = rankPagerankList.foldLeft(Double.MinValue)(Math.max(_, _))
+        val indegreePagesMinmaxMap = minMax(pagesIndegreeRank)
+        val pagerankPagesMinmaxMap = minMax(pagesPageRank)
 
         //Create rankedWebpageMap
+        val rankedEqualPagesMap = createRankedPageMap(pagesEqualRank, pages)
+        val rankedIndegreePagesMap = createRankedPageMap(pagesIndegreeRank, pages)
+        val rankedPagerankPagesMap = createRankedPageMap(pagesPageRank, pages)
+
+        //Initalize variables
+        var query = ""
+
+        //Take first query
+        println("Please enter a search query (:quit to leave program):")
+        query = readLine()
 
         //While query isn't :quit, do search loop
-            //Accept user query
-            //TODO: find immutable variable way to accept queries
-
+        while(query != ":quit"){
             //Compute how well each page matches using the methods of the PageSearch object
 
             //Min-Max Normalization of Match Ratings
 
             //Compute the overall match as the arithmetic mean of the pages rank and text-match
+
             //Compute the overall match as the geometric mean of the pages rank and text-match
+
             //Compute the overall match as the harmonic mean of the pages rank and text-match
+
 
             //Sort pages based on their overall match using scala.math.Ordering to support multiple options for computing the mean
 
+
             //Create searchedWebPage List
 
+
             //Display the name and url of the top 10 results
+
+
+            //Accept user query
+            println("Please enter a search query (:quit to leave program):")
+            query = readLine()
+        }
+
+
+
+
     }
 
-    // Load a List of WebPage objects from the packaged prolandwiki.csv file
+    // Load a List of WebPage objects from the packaged prolangwiki.csv file
     def loadWebPages: List[WebPage] = {
         // create an input stream to the proglangwiki.csv
         val fh = Source.fromInputStream(
@@ -66,5 +85,15 @@ object AskWillie {
     // Convert a List[WebPage] to a Map[String, WebPage]
     def mapWebPages(pages: List[WebPage]): Map[String, WebPage] = {
         (for (page <- pages) yield (page.id, page)).toMap
+    }
+
+    def minMax(pages: Map[String, Double]){
+        val min = pages.values.min
+        val max = pages.values.max
+        (for((page, weight) <- pages) yield (page, (weight - min)/(max - min))).toMap
+    }
+
+    def createRankedPageMap(rankMap: Map[String, Double], pages: Map[String, WebPage]): Map[String, RankedWebPage] ={
+        (for((page, weight) <- rankMap) yield (page, new RankedWebPage(page, pages(page).name, pages(page).url, pages(page).text, pages(page).links, weight))).toMap
     }
 }
--- src/main/scala/WebPages.scala
+++ src/main/scala/WebPages.scala
@@ -1,24 +1,27 @@
-class WebPage(val id: String, val name: String, val url: String,
-              val text: String, val links: List[String]) {
+class WebPage(val id: String,
+              val name: String,
+              val url: String,
+              val text: String,
+              val links: List[String]) {
 
 }
 
-class RankedWebPage (override val id: String,
-                     override val name: String,
-                     override val url: String,
-                     override val text: String,
-                     override val links: List[String],
-                     val weight: Double) extends WebPage (id, name, url, text, links){
+class RankedWebPage(override val id: String,
+                    override val name: String,
+                    override val url: String,
+                    override val text: String,
+                    override val links: List[String],
+                    val weight: Double) extends WebPage (id, name, url, text, links){
 
 }
 
-class SearchedWebPage (override val id: String,
-                       override val name: String,
-                       override val url: String,
-                       override val text: String,
-                       override val links: List[String],
-                       override val weight: Double,
-                       val textmatch: Double) extends RankedWebPage (id, name, url, text, links, weight){
+class SearchedWebPage(override val id: String,
+                      override val name: String,
+                      override val url: String,
+                      override val text: String,
+                      override val links: List[String],
+                      override val weight: Double,
+                      val textmatch: Double) extends RankedWebPage (id, name, url, text, links, weight){
 
 }
 
