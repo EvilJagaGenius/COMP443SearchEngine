bd5ad04f09e057ab9012664d8e2d77ed3365d1d2
Quinn Cypher <TMQCypher.git@gmail.com>
2021-04-15 11:14:35 -0400

    Implemented indegree weighting

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -6,10 +6,37 @@ object PageRank {
     def equal(pages: Map[String, WebPage]): Map[String, Double] =
       (for(s <- pages.keys) yield (s -> 1.0)).toMap
 
+
+
     def indegree(pages: Map[String, WebPage]): Map[String, Double] = {
-        // TODO: complete implementation
-        (for(s <- pages.keys) yield (s -> 1.0)).toMap
+      // TODO: complete implementation
+
+      // Hold all of the links in an iterable
+      val allLinks = for((id, page) <- pages; link <- page.links) yield link
+
+
+      def mapbuilder(
+        links: Iterable[String],
+        result: Map[String, Double]
+      ): Map[String, Double] = links match {
+
+        // If there are no more links, return the result
+        case Nil => result
+
+
+        // Otherwise, make the recursive call on a new result map with
+        // the head link's entry incremented by one
+        case head :: tail => {
+          val weight: Double = result.getOrElse(head, 0)
+          mapbuilder(tail, (result + (head -> (weight+1))))
+        }
+      }
+
+      mapbuilder(allLinks, Map[String, Double]())
+
     }
+
+
 
     def pagerank(pages: Map[String, WebPage]): Map[String, Double] = {
         // TODO: complete implementation
